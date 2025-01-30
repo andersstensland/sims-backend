ca933eee32f326b92ee30b63590ff5559939c155
Anders Stensland <43165915+andersstensland@users.noreply.github.com>
2025-01-30 11:03:48 +0100

    Init

--- src/main/java/org/kristiania/smartinventorymanagementsystem/config/DataSourceConfig.java
+++ src/main/java/org/kristiania/smartinventorymanagementsystem/config/DataSourceConfig.java
@@ -1,4 +1,17 @@
 package org.kristiania.smartinventorymanagementsystem.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import javax.sql.DataSource;
+
+@Configuration
 public class DataSourceConfig {
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(@Autowired DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
 }
